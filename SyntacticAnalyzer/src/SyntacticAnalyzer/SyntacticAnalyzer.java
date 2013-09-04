7e78f3119daf7bc933d604491c45fc4abddf3498
Victoria Martínez de la Cruz <victoria@vmartinezdelacruz.com>
2013-09-04 13:17:41 -0300

    Creacion del proyecto SyntacticAnalyzer. Simplemente se tomaron las clases del analizador lexico que seran utilizadas en esta etapa. ¡A darle masa a la etapa 2!

---
+++ SyntacticAnalyzer/src/SyntacticAnalyzer/SyntacticAnalyzer.java
@@ -0,0 +1,17 @@
+package SyntacticAnalyzer;
+
+/**
+ * Clase principal del analizador sintáctico.
+ *
+ * @author Ramiro Agis
+ * @author Victoria Martínez de la Cruz
+ */
+public class SyntacticAnalyzer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
