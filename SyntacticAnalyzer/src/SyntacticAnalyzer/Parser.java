8ce23619e4e1f344991e4a05fdfa86eb97ccf5b3
Victoria Martínez de la Cruz <victoria@vmartinezdelacruz.com>
2013-09-04 19:15:16 -0300

    Metodos para cada no-terminal

--- SyntacticAnalyzer/src/SyntacticAnalyzer/Parser.java
+++ SyntacticAnalyzer/src/SyntacticAnalyzer/Parser.java
@@ -7,28 +7,163 @@ package SyntacticAnalyzer;
  * @author Victoria Martínez de la Cruz
  */
 public class Parser {
-    private void Class() throws LexicalException, SyntacticException {
-        
+
+    private void Inicial() throws LexicalException, SyntacticException {
+    }
+
+    private void ListaClases() throws LexicalException, SyntacticException {
+    }
+
+    private void Clase() throws LexicalException, SyntacticException {
+    }
+
+    private void Herencia() throws LexicalException, SyntacticException {
+    }
+
+    private void ListaMiembros() throws LexicalException, SyntacticException {
+    }
+
+    private void Miembro() throws LexicalException, SyntacticException {
+    }
+
+    private void Atributo() throws LexicalException, SyntacticException {
+    }
+
+    private void Metodo() throws LexicalException, SyntacticException {
+    }
+
+    private void Ctor() throws LexicalException, SyntacticException {
+    }
+
+    private void ArgsFormales() throws LexicalException, SyntacticException {
+    }
+
+    private void ListaArgsFormales() throws LexicalException, SyntacticException {
+    }
+
+    private void ListaArgsFormales_() throws LexicalException, SyntacticException {
+    }
+
+    private void ArgFormal() throws LexicalException, SyntacticException {
+    }
+
+    private void VarsLocales() throws LexicalException, SyntacticException {
+    }
+
+    private void ListaAtributos() throws LexicalException, SyntacticException {
+    }
+
+    private void ModMetodo() throws LexicalException, SyntacticException {
+    }
+
+    private void TipoMetodo() throws LexicalException, SyntacticException {
+    }
+
+    private void Tipo() throws LexicalException, SyntacticException {
+    }
+
+    private void TipoPrimitivo() throws LexicalException, SyntacticException {
+    }
+
+    private void ListaDecVars() throws LexicalException, SyntacticException {
+    }
+
+    private void Bloque() throws LexicalException, SyntacticException {
+    }
+
+    private void ListaSentencias() throws LexicalException, SyntacticException {
+    }
+
+    private void Sentencia() throws LexicalException, SyntacticException {
+    }
+
+    private void Sentencia_() throws LexicalException, SyntacticException {
+    }
+
+    private void Asignacion() throws LexicalException, SyntacticException {
+    }
+
+    private void SentenciaSimple() throws LexicalException, SyntacticException {
+    }
+
+    private void Expresion() throws LexicalException, SyntacticException {
     }
     
-//    private void Class() throws LexicalException, SyntacticException {
-//        
-//    }
-//    
-//    private void Class() throws LexicalException, SyntacticException {
-//        
-//    }
-//    
-//    private void Class() throws LexicalException, SyntacticException {
-//        
-//    }
-//    
-//    private void Class() throws LexicalException, SyntacticException {
-//        
-//    }
-//    
-//    private void Class() throws LexicalException, SyntacticException {
-//        
-//    }
- 
+    private void Expresion6() throws LexicalException, SyntacticException {
+    }
+
+    private void RExpresion6() throws LexicalException, SyntacticException {
+    }
+
+    private void Expresion5() throws LexicalException, SyntacticException {
+    }
+
+    private void RExpresion5() throws LexicalException, SyntacticException {
+    }
+
+    private void Expresion4() throws LexicalException, SyntacticException {
+    }
+
+    private void RExpresion4() throws LexicalException, SyntacticException {
+    }
+    
+    private void Expresion3() throws LexicalException, SyntacticException {
+    }
+
+    private void RExpresion3() throws LexicalException, SyntacticException {
+    }
+    
+    private void Expresion2() throws LexicalException, SyntacticException {
+    }
+
+    private void RExpresion2() throws LexicalException, SyntacticException {
+    }
+    
+    private void Expresion1() throws LexicalException, SyntacticException {
+    }
+
+    private void RExpresion1() throws LexicalException, SyntacticException {
+    }    
+
+    private void Expresion0() throws LexicalException, SyntacticException {
+    }
+
+    private void Operador4() throws LexicalException, SyntacticException {
+    }
+
+    private void Operador3() throws LexicalException, SyntacticException {
+    }
+    
+    private void Operador2() throws LexicalException, SyntacticException {
+    }    
+    
+    private void Operador1() throws LexicalException, SyntacticException {
+    }    
+    
+    private void OperadorUnario() throws LexicalException, SyntacticException {
+    }    
+    
+    private void Primario() throws LexicalException, SyntacticException {
+    }
+    
+    private void ListaLlamadas_() throws LexicalException, SyntacticException {
+    }
+    
+    private void ListaLlamadas() throws LexicalException, SyntacticException {
+    }
+
+    private void Llamada() throws LexicalException, SyntacticException {
+    }
+    
+    private void Literal() throws LexicalException, SyntacticException {
+    }
+    
+    private void ArgsActuales() throws LexicalException, SyntacticException {
+    }
+    
+    private void ListaExps() throws LexicalException, SyntacticException {
+    }
+    
+    private void ListaExps_() throws LexicalException, SyntacticException {
+    }
 }
