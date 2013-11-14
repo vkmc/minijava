ac8a520cf43896f890c124f2b8436a7fcca4a455
Rama <Rama@Rama-PC>
2013-11-13 23:27:12 -0300

    DERP, ahi lo fixié bien

--- IntermediateCodeGeneration/src/IntermediateCodeGeneration/AST/AssignNode.java
+++ IntermediateCodeGeneration/src/IntermediateCodeGeneration/AST/AssignNode.java
@@ -126,8 +126,7 @@ public class AssignNode extends SentenceNode {
             } else if (currentMethodEntry.getParameterEntry(id.getLexeme()) != null) {
                 // el offset de una variable local comienza en 0 y decrece
                 int parameterOffset = currentMethodEntry.getParameterEntry(id.getLexeme()).getOffset();
-                System.out.println(parameterOffset + 3);
-                ICG.GEN("STORE", parameterOffset, "Asignacion. El lado izquierdo es un parametro del metodo '" + currentService + "'");
+                ICG.GEN("STORE", parameterOffset + 3, "Asignacion. El lado izquierdo es un parametro del metodo '" + currentService + "'");
             } else if (currentClassEntry.getInstanceVariableEntry(id.getLexeme()) != null) {
                 ICG.GEN("LOAD", 3, "Asignacion. Apilamos THIS");
                 ICG.GEN("SWAP", "Asignacion. Invertimos el orden del tope de la pila. STOREREF usa los parametros en orden inverso (CIR, valor).");
